f61a2c5cba05e8252d6c0bb307af2a33165bb40c
me76ss <me76ss@ymail.com>
2018-09-11 17:29:57 +0430

    debug t to f

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's%@szg6yc=1gmhnbz5ak$15(d&x=q)fkf@mtdjks&gsyx_0*c('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
